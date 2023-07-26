256630c92f77385b7fa97233e3084349b9798f11
pedrotocabj <pedrovido9@gmail.com>
2023-07-26 17:03:34 -0300

    cambios de nombres

--- canciones.js
+++ canciones.js
@@ -1,25 +1,31 @@
 const canciones = [
   {
+      nombre:"Cancion Para Mi Muerte",
     link: "https://open.spotify.com/embed/track/0t0Kl5jxxV3s8bdpILkgmd?utm_source=generator",
     acordes: "",
   },
   {
+      nombre:"Rasguña las Piedras",
     link: "https://open.spotify.com/embed/track/3lvAzKmRH8vzXQ4xm19v78?utm_source=generator",
     acordes: "",
   },
   {
+      nombre:"Cuabdo te Vayas",
     link: "https://open.spotify.com/embed/track/2s8089EmkUwB44RzBdKX8u?utm_source=generator",
     acordes: "",
   },
   {
+      nombre:"Bienvenidos al Tren",
     link: "https://open.spotify.com/embed/track/1aXdFFTTUBMdcKsAX2q3vq?utm_source=generator",
     acordes: "",
   },
   {
+      nombre:"No Tengo Ganas",
       link:"https://open.spotify.com/embed/track/5LIHbW6EG9IRlIaXfcuFnH?utm_source=generator",
       acordes:"",
   },
   {
+      nombre:"Alto en la Torre",
     link: "https://open.spotify.com/embed/track/28sGklsX9Awf4FYqYZNgQN?utm_source=generator",
     acordes: "",
   },
---
+++ lista.js
@@ -0,0 +1,65 @@
+const contenedorCancionesLista = document.getElementById(
+    "contenedor-canciones-lista"
+  );
+  
+  
+  
+  function crearCancionInicio(canciones) {
+    canciones.forEach((cancion) => {
+      const nuevaCancion = document.createElement("div");
+      nuevaCancion.classList = "conteiner-cancion-objeto";
+      nuevaCancion.innerHTML = `
+            <div class="iframe-spotify">
+            <iframe style="border-radius:12px" src="${cancion.link}" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+            <div class="boton-borrar">
+            <button class="eliminar-btn"><i class="fa-solid fa-trash-can"></i></button>
+            </div>
+      `;
+      contenedorCancionesLista.appendChild(nuevaCancion);
+    });
+  }
+  
+  crearCancionInicio(canciones);
+  
+  function agregarCancion() {
+    const lastItem = canciones[canciones.length - 1];
+    const nuevaCancion = document.createElement("div");
+    nuevaCancion.classList = "conteiner-cancion-objeto";
+    nuevaCancion.innerHTML = `
+    <div class="iframe-spotify">
+    <iframe style="border-radius:12px" src="${lastItem.link}" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+    <div class="boton-borrar">
+    <button class="eliminar-btn"><i class="fa-solid fa-trash-can"></i></button>
+    </div>
+      `;
+    contenedorCancionesLista.appendChild(nuevaCancion);
+  }
+  
+  const botonSubmit = document.getElementById("btn-agregar");
+  const form = document.querySelector("form");
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formulario = getFormulario(e);
+    canciones.push(formulario);
+    agregarCancion();
+    form.reset();
+  });
+  
+  function getFormulario(e) {
+    return {
+      link: e.target.elements.link.value,
+      acordes: e.target.elements.acordes.value,
+    };
+  }
+  
+  const botonBorrar = document.getElementsByClassName("eliminar-btn");
+  Array.from(botonBorrar).forEach((boton) => {
+    boton.addEventListener("click", () => {
+      const divSeleccionado = boton.closest(".conteiner-cancion-objeto");
+      divSeleccionado.remove();
+    });
+  });
+  
