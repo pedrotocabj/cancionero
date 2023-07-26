251b2b50300e43459012b977b005b06e8d13f6c6
pedrotocabj <pedrovido9@gmail.com>
2023-07-26 16:52:15 -0300

    nuevo array

--- canciones.js
+++ canciones.js
@@ -1,14 +1,26 @@
 const canciones = [
   {
-        link:'https://open.spotify.com/embed/track/0t0Kl5jxxV3s8bdpILkgmd?utm_source=generator',
-        acordes:""
+    link: "https://open.spotify.com/embed/track/0t0Kl5jxxV3s8bdpILkgmd?utm_source=generator",
+    acordes: "",
   },
   {
-        link:'https://open.spotify.com/embed/track/3lvAzKmRH8vzXQ4xm19v78?utm_source=generator',
-        acordes:""
+    link: "https://open.spotify.com/embed/track/3lvAzKmRH8vzXQ4xm19v78?utm_source=generator",
+    acordes: "",
   },
   {
-        link:'https://open.spotify.com/embed/track/34NGjr3W7K9E484f7C5xVf?utm_source=generator',
-        acordes:""
-  }
-]
+    link: "https://open.spotify.com/embed/track/2s8089EmkUwB44RzBdKX8u?utm_source=generator",
+    acordes: "",
+  },
+  {
+    link: "https://open.spotify.com/embed/track/1aXdFFTTUBMdcKsAX2q3vq?utm_source=generator",
+    acordes: "",
+  },
+  {
+      link:"https://open.spotify.com/embed/track/5LIHbW6EG9IRlIaXfcuFnH?utm_source=generator",
+      acordes:"",
+  },
+  {
+    link: "https://open.spotify.com/embed/track/28sGklsX9Awf4FYqYZNgQN?utm_source=generator",
+    acordes: "",
+  },
+];
